b75e770e00a799f0f392729e1061b85763091fd3
jekanito <jekaezil@gmail.com>
2019-07-30 17:14:26 +0300

    Добавление файла теста TicketsApiTest

---
+++ frontend/tests/unit/models/TicketsApiTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Жека
+ * Date: 30.07.2019
+ * Time: 17:10
+ */
+
+namespace frontend\tests\unit\models;
+
+use common\fixtures\UserFixture;
+use frontend\models\SignupForm;
+
+use common\models\Tickets;
+
+class TicketsApiTest extends \Codeception\Test\Unit
+{
+    protected $tester;
+
+    
+}
